922eb6b80d90fd88565646283a23593dd8721950
EmerArte <48594349+EmerArte@users.noreply.github.com>
2020-05-24 21:01:40 -0500

    ESTO ES LO QUE HICE POR HOY MAÑANA TERMINO TODO LO QUE TIENE QUE VER CON CLIENTE, MAÑANA PUEDES PROGRAMAR QUE EL LOGIN HAGA ALGO

--- app/src/main/java/com/example/cecyapp/layout/LayoutCliente.java
+++ app/src/main/java/com/example/cecyapp/layout/LayoutCliente.java
@@ -1,4 +1,23 @@
 package com.example.cecyapp.layout;
 
-public class LayoutCliente {
+import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cecyapp.R;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class LayoutCliente extends AppCompatActivity {
+    public Toolbar toolbar;
+    public TabItem tabItem;
+    public TabLayout tabLayout;
+    public ScrollView Scrollview;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_cliente);
+
+}
 }
