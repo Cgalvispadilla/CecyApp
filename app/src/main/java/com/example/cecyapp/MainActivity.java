ba61286b524f653657fbd81fef0a8197cdbdbe10
Carlos Andres Galvis Padilla <48323392+Cgalvispadilla@users.noreply.github.com>
2020-05-24 23:42:44 -0500

    F

--- app/src/main/java/com/example/cecyapp/MainActivity.java
+++ app/src/main/java/com/example/cecyapp/MainActivity.java
@@ -15,6 +15,8 @@ import com.example.cecyapp.layout.LayoutCliente;
 import com.example.cecyapp.layout.LayoutModista;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -24,12 +26,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 
-import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
     private TextView  tvRegistro;
-    private TextView tvCorreoEle;
-    private TextView tvContraseña;
+    private TextInputEditText tvCorreoEle;
+    private TextInputEditText tvContraseña;
     private Button btnSesion;
 
     private String correo = "";
@@ -45,21 +47,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvRegistro = (TextView) findViewById(R.id.tv_registro);
-        tvCorreoEle = (TextView) findViewById(R.id.imput_usuario);
-        tvContraseña = (TextView) findViewById(R.id.imput_contra);
+        tvCorreoEle = (TextInputEditText) findViewById(R.id.imput_usuario);
+        tvContraseña = (TextInputEditText) findViewById(R.id.imput_contra);
         btnSesion = (Button) findViewById(R.id.but_iniciarsesion);
+
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+
+
+
 
         btnSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 correo = tvCorreoEle.getText().toString();
                 contraseña = tvContraseña.getText().toString();
+
                 if (!correo.isEmpty() && !contraseña.isEmpty()) {
                     ingresarUsuario();
+
                 } else {
-                    Toast.makeText(getApplicationContext(), "Debe llenar todos los campos", Toast.LENGTH_LONG);
+                    Toast.makeText(getApplicationContext(), "Debe llenar todos los campos", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -73,11 +83,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void ingresarUsuario() {
+
         mAuth.signInWithEmailAndPassword(correo, contraseña).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isComplete()) {
                     String id=mAuth.getCurrentUser().getUid();
+
                     mDatabase.child("cliente").child(id).addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -96,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot1) {
                             if(dataSnapshot1.exists()){
-                                //se abre la interfaz modista
+
                                 startActivity(new Intent(getApplicationContext(), LayoutModista.class));
 
                             }
@@ -109,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
                     });
                     //hay que abrir otro activity
                 } else {
-                    Toast.makeText(getApplicationContext(), "¡Error Al iniciar sesión!, compruebe sus datos", Toast.LENGTH_LONG);
+                    Toast.makeText(getApplicationContext(), "¡Error Al iniciar sesión!, compruebe sus datos", Toast.LENGTH_LONG).show();
                 }
             }
         });
